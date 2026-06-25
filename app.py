4a4ba5aa58bd4cbaa08b99cebe2790a68a73a006
anilthirtha-bit <anilthirtha@gmail.com>
2026-06-25 13:27:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sentiment Analysis</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Sentiment Analysis</h1>
+
+    <textarea id="text" placeholder="Enter text"></textarea>
+
+    <br><br>
+
+    <button onclick="analyzeSentiment()">
+        Analyze
+    </button>
+
+    <h2 id="result"></h2>
+</div>
+
+<script src="script.js"></script>
+
+</body>
+</html>
